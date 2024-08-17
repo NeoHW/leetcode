26c661d4c177f489a3b7661c051b06054dab7bdc
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-08-18 04:19:09 +0800

    Time: 3 ms (99.01%) | Memory: 19.4 MB (36.36%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        // recursive relation: minJumpsToEnd[i] = 1 + min(minJumpsToEnd[i+1], minJumpsToEnd[i+2] ... minJumpsToEnd[i+k]) : where k = nums[i]
         int n = nums.size();
-        vector<int> dp(n, INT_MAX);
-        
-        // base case: no need to jump when you're already there
-        dp[n-1] = 0;
+        // furthest track furthest index we can reach from current index
+        // end indicates end of current jump range
+        int jumps = 0, furthest = 0, end = 0;
 
-        for (int i = n-2; i >= 0; i--) {
-            int minOfAllAfter = INT_MAX;
-            for (int j = i+1; j <= i+nums[i] && j < n; j++) {
-                minOfAllAfter = min(minOfAllAfter, dp[j]);
-            }
-            if (minOfAllAfter != INT_MAX) {
-                dp[i] = 1 + minOfAllAfter;
+        for (int i = 0; i < n - 1; i++) {
+            furthest = max(furthest, i + nums[i]);
+            if (i == end) {
+                jumps++;
+                end = furthest;
             }
         }
 
-        return dp[0];
+        return jumps;
     }
 };
