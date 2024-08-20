a08ffa19fb3d67e2767b447eb53336816a46fb6a
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-08-21 00:10:17 +0800

    Time: 0 ms (100.00%) | Memory: 9.4 MB (68.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -3,7 +3,13 @@ public:
     int rob(vector<int>& nums) {
         // use "house robber 1" to run from subarray[0,n-2] and subarray[1,n-1]
         int n = nums.size();
-        return max({nums[0], robsub(nums, 0, n-2), robsub(nums, 1, n-1)});
+
+        // consider edge case where there is only one element
+        if (n == 1) {
+            return nums[0];
+        }
+
+        return max(robsub(nums, 0, n-2), robsub(nums, 1, n-1));
     }
 
 private:
