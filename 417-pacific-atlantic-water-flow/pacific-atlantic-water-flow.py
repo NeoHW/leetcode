d6239229912cbc204e82ddbce3b78b7bf27fafa7
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-09-07 17:54:18 +0800

    Time: 231 ms (23.92%) | Memory: 18.2 MB (33.66%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,42 +1,26 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # have 2 queues, one for pacific, one for atlantic, find common nodes
+        # 2 sets containing reachable points from atl and pac respectively
         m,n = len(heights), len(heights[0])
         dir = [(0,1), (1,0), (0,-1), (-1,0)]
+        pac, atl = set(), set()
 
-        q1 = deque() # for pacific
-        q2 = deque() # for atlantic
-        visited_q1 = [n*[0] for i in range(m)]
-        visited_q2 = [n*[0] for i in range(m)]
+        def dfs(i ,j, visited, prevHeight):
+            if (i,j) in visited or i not in range(m) or j not in range(n) or heights[i][j] < prevHeight:
+                return
+            
+            visited.add((i,j))
 
+            for dx,dy in dir:
+                dfs(i+dx, j+dy, visited, heights[i][j])
+        
         for i in range(m):
-            q1.append((i,0))
-            q2.append((i,n-1))
-            visited_q1[i][0] = 1
-            visited_q2[i][n-1] = 1
+            dfs(i, 0, pac, heights[i][0]) # pacific
+            dfs(i, n-1, atl, heights[i][n-1]) # atlantic
         
         for i in range(n):
-            q1.append((0,i))
-            q2.append((m-1,i))
-            visited_q1[0][i] = 1
-            visited_q2[m-1][i] = 1
+            dfs(0, i, pac, heights[0][i]) # pacific
+            dfs(m-1, i, atl, heights[m-1][i]) # atlantic
 
-        def bfs(q, visited):
-            while q:
-                x,y = q.popleft()
-                for dx,dy in dir:
-                    newx, newy = dx+x, dy+y
-                    if newx in range(m) and newy in range(n) and heights[x][y] <= heights[newx][newy] and not visited[newx][newy]:
-                        q.append((newx,newy))
-                        visited[newx][newy] = 1
-
-        bfs(q1, visited_q1)
-        bfs(q2, visited_q2)
-        
-        res = []
-        for i in range(m):
-            for j in range(n):
-                if visited_q1[i][j] == 1 and visited_q2[i][j] == 1:
-                    res.append([i,j])
-        
-        return res
+        # find intersection using &
+        return pac & atl
