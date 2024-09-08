66a1a527e57f7f4ce63649821094b417cf63044a
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-09-08 16:54:10 +0800

    Time: 49 ms (90.54%) | Memory: 16.5 MB (87.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,20 +3,32 @@ class Solution:
         len_s1 = len(s1)
         len_s2 = len(s2)
 
-        hm_s1 = Counter(s1)
-        hm_s2 = Counter(s2[:len_s1])
+        if len(s1) > len(s2): 
+          return False
+        
+        hm_s1 = defaultdict(int)
+        hm_s2 = defaultdict(int)
 
+        # populate hashmap for first window
+        for i in range(len_s1):
+            hm_s1[s1[i]] += 1
+            hm_s2[s2[i]] += 1
+
+        # check first window
+        if hm_s1 == hm_s2:
+            return True
+        
         l = 0
+        for r in range(len_s1, len_s2):
+            hm_s2[s2[l]] -= 1
+            if hm_s2[s2[l]] == 0:
+                del hm_s2[s2[l]]
+            
+            hm_s2[s2[r]] += 1
 
-        for r in range(len_s1, len_s2 + 1): # +1 here to do checking for last element too
             if hm_s1 == hm_s2:
                 return True
-
-            if r < len_s2:
-                hm_s2[s2[l]] -= 1
-                if hm_s2[s2[l]] == 0:
-                    del hm_s2[s2[l]] # clean up those with 0 freq
-                l += 1
-                hm_s2[s2[r]] += 1
+            
+            l += 1
                 
         return False
