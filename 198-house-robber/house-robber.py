ad4301b13ba2a8cc5d866defa356f09f387a6d40
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-09-12 02:23:57 +0800

    Time: 40 ms (24.85%) | Memory: 16.5 MB (69.22%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # rob every alt house starting from i = 0 or i = 1
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+
+        prev, curr = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            temp = max(nums[i] + prev, curr)
+            prev = curr
+            curr = temp
+        
+        return curr
