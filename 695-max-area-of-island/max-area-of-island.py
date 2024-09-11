1957bf0a80f6a666ec5a9b568264c1f23bc1faa4
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-09-12 01:40:00 +0800

    Time: 122 ms (22.70%) | Memory: 17.2 MB (63.94%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,27 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m,n = len(grid), len(grid[0])
-        visited = [n * [0] for _ in range(m)]
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
-        max_area = 0
+        maxArea = 0 
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        
+        visited = set()
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1 and not visited[i][j]:
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    # do bfs here
+                    count = 0
                     q = deque()
                     q.append((i,j))
-                    visited[i][j] = 1
-                    area = 1
+                    visited.add((i,j))
 
                     while q:
-                        x,y = q.popleft()
+                        x,y = q.pop()
+                        count += 1
                         for dx,dy in directions:
-                            newx, newy = x+dx, y+dy
-                            if newx in range(m) and newy in range(n) and grid[newx][newy] == 1 and not visited[newx][newy]:
-                                q.append((newx,newy))
-                                visited[newx][newy] = 1
-                                area += 1
-                    
-                    max_area = max(max_area,area)
+                            newx, newy = x + dx, y + dy
+                            if newx in range(m) and newy in range(n) and grid[newx][newy] == 1 and (newx, newy) not in visited:
+                                q.append((newx, newy))
+                                visited.add((newx, newy))
+
+                    maxArea = max(maxArea, count)
         
-        return max_area
+        return maxArea
