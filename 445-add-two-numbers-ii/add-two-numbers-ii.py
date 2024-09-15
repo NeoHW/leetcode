dc89b06b40fbd2eec2c780bf2be69bb0cd0b0c21
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-09-16 03:05:46 +0800

    Time: 43 ms (98.47%) | Memory: 16.5 MB (96.38%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -6,35 +6,30 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         # reverse LLs, or use a stack
-        l1 = self.reverse(l1)
-        l2 = self.reverse(l2)
+        stack1, stack2 = [], []
 
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
+        
         carry = 0
-        dummy = curr = ListNode()
+        head = None
 
-        while l1 or l2 or carry != 0:
-            digit1 = l1.val if l1 else 0
-            digit2 = l2.val if l2 else 0
+        while stack1 or stack2 or carry:
+            digit1 = stack1.pop() if stack1 else 0
+            digit2 = stack2.pop() if stack2 else 0
 
             total = digit1 + digit2 + carry
-            val, carry = total % 10, total // 10
-            curr.next = ListNode(val)
+            val = total % 10
+            carry = total // 10
 
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            curr = curr.next
+            # instead of appending nodes m preprend them to the head of the list
+            new_node = ListNode(val)
+            new_node.next = head
+            head = new_node
 
-        return self.reverse(dummy.next)
-    
-
-    def reverse(self, head):
-        curr = head
-        prev = None
-
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        
-        return prev
+        return head
