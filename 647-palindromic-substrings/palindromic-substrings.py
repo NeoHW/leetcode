932168c2c6de9eb16c295f82fc501132ce7aca0f
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-09-18 13:45:20 +0800

    Time: 227 ms (34.46%) | Memory: 24.5 MB (22.16%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -2,13 +2,13 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
         count = 0
-        dp = [n * [0] for _ in range(n)]
+        dp = [[0] * n for _ in range(n)]
 
         for i in range(n):
-            dp[i][i] = 1
+            dp[i][i] = 1 # one letter is a palindrome
             count += 1
             for j in range(i):
-                if s[j] == s[i] and (i-j <= 2 or dp[j+1][i-1]):
+                if s[i] == s[j] and (i-j < 2 or dp[j+1][i-1]):
                     dp[j][i] = 1
                     count += 1
         
