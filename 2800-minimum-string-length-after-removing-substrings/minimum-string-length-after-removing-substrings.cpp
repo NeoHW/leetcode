3b2807cea2ed10e6c9431bf65c5d890eed7d22d0
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-10-08 01:56:27 +0800

    Time: 7 ms (51.96%) | Memory: 10 MB (93.92%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int minLength(string sentence) {
+        // two pointers method, O(1) space
+        int writePtr = 0;
+
+        for (int readPtr = 0; readPtr < sentence.size(); ++readPtr) {
+            // replace curr writePtr char with readPtr char
+            sentence[writePtr] = sentence[readPtr];
+
+            // check writePtr-1 and readPtr for valid removal pattern
+            if (writePtr > 0 && 
+                (sentence[writePtr-1] == 'A' || sentence[writePtr-1] == 'C') &&
+                sentence[writePtr] == sentence[writePtr-1] + 1) {
+                    --writePtr;
+                } else {
+                    ++writePtr;
+                }
+        }
+        
+        return writePtr;
+        
+        
+        /*  
         // use a stack , similar to matching parenthesis
         std::stack<char> s;
 
@@ -16,7 +37,7 @@ public:
                 s.push(c);
             }
         }
-
         return s.size();
+        */
     }
 };
