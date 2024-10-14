c17f8bd00ad2bf25817480c202dea9e2abde0554
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-10-15 03:09:36 +0800

    Time: 221 ms (81.21%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,10 @@
 -- Write your PostgreSQL query statement below
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
-WHERE w1.temperature > w2.temperature;
+SELECT current_day.id
+FROM Weather as current_day
+WHERE EXISTS (
+    SELECT 1
+    FROM Weather AS yesterday
+    WHERE current_day.recordDate = yesterday.recordDate + 1
+    AND current_day.temperature > yesterday.temperature
+)
+;
