f19bcbf17777d6cc67a87d3d90d02966ca0b2f03
Haowei <94470439+NeoHW@users.noreply.github.com>
2024-12-11 21:24:15 +0800

    Time: 225 ms (53.84%) | Memory: 28 MB (45.21%) - LeetSync

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        # find the longest subarray in range (i,j) that 
+        left = 0
+        max_window = 0
+
+        for right in range(len(nums)):
+            while nums[right] - nums[left] > 2 * k:
+                left += 1
+            max_window = max(max_window, right - left + 1)
+
+        return max_window
