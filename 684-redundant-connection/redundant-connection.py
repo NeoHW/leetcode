9cff6f182d41f9e042b27212c5be48924d2e3c52
Haowei <94470439+NeoHW@users.noreply.github.com>
2025-01-30 04:42:37 +0800

    Time: 0 ms (100.00%) | Memory: 18.3 MB (34.99%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,32 +1,30 @@
 class UnionFind:
     def __init__(self, n):
         self.count = n
-        self.parent = {i:-1 for i in range(1, n+1)} # 1 based indexing in question!
-    
+        self.parents = {i:-1 for i in range(1,n+1)} 
+
     def find(self, node):
-        if self.parent[node] == -1:
+        if self.parents[node] == -1:
             return node
         
-        root = self.find(self.parent[node])
-        self.parent[node] = root
+        root = self.find(self.parents[node])
+        self.parents[node] = root
         return root
-
+    
     def union(self, n1, n2):
         p1 = self.find(n1)
         p2 = self.find(n2)
-        
-        if p1 != p2:
-            self.parent[p2] = p1
-            self.count -= 1
-            return True
-        
-        return False
 
+        if p1 == p2:
+            return False
+        
+        self.parents[p2] = p1
+        self.count -= 1
+        return True
+    
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        uf = UnionFind(n)
-
+        uf = UnionFind(len(edges)) 
         for v,w in edges:
             if not uf.union(v,w):
                 return [v,w]
