1d88ae761b708b1a99df455aceee57310dd6c65c
Haowei <94470439+NeoHW@users.noreply.github.com>
2025-03-20 02:24:51 +0800

    Time: 157 ms (34.45%) | Memory: 21.7 MB (54.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -7,17 +7,9 @@ class Solution:
             if num == 0 and i <= n - 3:
                 res += 1
                 for j in range(i, i+3):
-                    nums[j] = self.flip(nums[j])
+                    nums[j] = nums[j] ^ 1  # bit manipulation
         
-        for i in range(n-3, n):
-            if nums[i] == 0:
-                return -1
-        
+        if nums[n - 2] == 0 or nums[n - 1] == 0:
+            return -1
+
         return res 
-
-
-    def flip(self, num:int) -> int:
-        if num == 0:
-            return 1
-        else:
-            return 0
