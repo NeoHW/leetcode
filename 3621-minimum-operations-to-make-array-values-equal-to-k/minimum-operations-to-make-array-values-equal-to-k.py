6c1bafef9f13f691e1659592d7e43ec909a4f3f6
Haowei <94470439+NeoHW@users.noreply.github.com>
2025-04-09 22:28:33 +0800

    Time: 65 ms (69.34%) | Memory: 17.9 MB (9.43%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,12 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         # basically return number of distinct elements greater than k
-        if min(nums) < k:
-            return -1
         
         hs = set()
+
         for num in nums:
-            if num > k:
+            if num < k:
+                return -1
+            elif num > k:
                 hs.add(num)
-        
+
         return len(hs)
