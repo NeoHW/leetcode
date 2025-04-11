864f3183bfd712e186d569e24be5649da10e55af
Haowei <94470439+NeoHW@users.noreply.github.com>
2025-04-12 00:29:42 +0800

    Time: 105 ms (96.94%) | Memory: 18 MB (13.03%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        res = 0
+
+        for a in range(low, high + 1):
+            if a < 100 and a % 11 == 0:
+                res += 1
+            
+            # only need to care 4 digit numbers
+            if 1000 <= a < 10000:
+                left = a // 1000 + a % 1000 // 100
+                right = a % 100 // 10 + a % 10
+                if left == right:
+                    res += 1
+        
+        return res
