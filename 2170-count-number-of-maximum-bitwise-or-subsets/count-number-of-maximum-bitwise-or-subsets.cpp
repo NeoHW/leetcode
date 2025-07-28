c7f0ba133f0ec15f08039b88db380c4bc7027554
Haowei <94470439+NeoHW@users.noreply.github.com>
2025-07-28 23:57:51 +0800

    Time: 107 ms (19.57%) | Memory: 310.1 MB (10.75%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -6,17 +6,23 @@ public:
         for (const int num: nums) {
             max_value |= num;
         }
-        return countSubsets(nums, 0, 0, max_value);
+        vector<vector<int>> memo (nums.size(), vector<int>(max_value + 1, -1)); // dp
+        return countSubsets(nums, 0, 0, max_value, memo);
     }
 
 private:
-    int countSubsets(vector<int>& nums, int index, int current, int target) {
+    int countSubsets(vector<int>& nums, int index, int current, int target, vector<vector<int>>& memo) {
         if (index == nums.size()) {
             return (current == target) ? 1 : 0;
         }
+
+        if (memo[index][current] != -1) {
+            return memo[index][current];
+        }
+
         // either pick or dont pick the current index 
-        int count_without = countSubsets(nums, index + 1, current, target);
-        int count_with = countSubsets(nums, index + 1, current | nums[index], target);
-        return count_without + count_with;
+        int count_without = countSubsets(nums, index + 1, current, target, memo);
+        int count_with = countSubsets(nums, index + 1, current | nums[index], target, memo);
+        return memo[index][current] = count_without + count_with;
     }
 };
