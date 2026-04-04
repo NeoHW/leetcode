57f042b6854a48e6b617d4983b39516eccdcb8eb
Haowei <94470439+NeoHW@users.noreply.github.com>
2026-04-04 15:46:59 +0800

    Time: 11 ms (75.89%) | Memory: 19.5 MB (35.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        hm = defaultdict(int)
 
-        return sorted(s) == sorted(t)
+        for c in s:
+            hm[c] += 1
+
+        for c in t:
+            hm[c] -= 1
+        
+        for v in hm.values():
+            if v != 0:
+                return False
+        
+        return True
