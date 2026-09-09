d382b867c2fd3a465c18439a0bbe939826970303
Haowei <94470439+NeoHW@users.noreply.github.com>
2026-09-10 02:12:18 +0800

    Time: 67 ms (20.09%) | Memory: 28.6 MB (76.31%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # kadane algo, find the largest subarray sum
-        curr_diff, max_so_far = 0,0
-
-        for i in range(1, len(prices)):
-            curr_diff += prices[i] - prices[i-1]
-            if curr_diff < 0: # found prices[i] lower than time we ought, buy here instead
-                curr_diff = 0
-            max_so_far = max(max_so_far, curr_diff)
-        
-        return max_so_far
+        res = 0
+        l = 0
+        for r in range(1,len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+                continue
+            
+            res = max(res, prices[r] - prices[l])
+        return res
